d3b30a611b182173595caf59378df0da0105149e
Carlos Vinicius <carlinhosb13@live.com>
2020-06-13 13:24:24 -0300

    feat: filter sql and scripts for inputs

--- php-action/update.php
+++ php-action/update.php
@@ -1,12 +1,20 @@
 <?php
 require_once "db_connect.php";
 session_start();
+function clear($input)
+{
+    global $connect;
+    //sql
+    $var = mysqli_escape_string($connect, $input);
+    //script
+    $var = htmlspecialchars($var);
+}
 if (isset($_POST['btn-atualizar'])) :
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
-    $id = mysqli_escape_string($connect, $_POST['id']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
+    $id = clear($_POST['id']);
     $sql = "UPDATE clientes SET nome = '$nome', sobrenome = '$sobrenome', email = '$email', idade = '$idade' WHERE id='$id'";
 
     if (mysqli_query($connect, $sql)) :
